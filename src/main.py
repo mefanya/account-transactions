b504a14173cf97e5b2017417ec016ba08c99ddab
mefanya <ms1604@bk.ru>
2024-05-12 14:58:12 +0300

    Correctly working program

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 import json
+from utils import filtered_and_sorted, get_data_correct_format
 
 
 def main():
     with open("/home/mefanya/PycharmProjects/account-transactions/data/operations.json", "r", encoding="utf-8") as file:
         data = json.load(file)
 
-    print(data)
+    print("Сколько последних операций хотите получить?")
+    user_input = int(input("Введите число: "))
+
+    item = filtered_and_sorted(data)
+
+    if user_input > len(item):
+        user_input = len(item)
+
+    for i in range(int(user_input)):
+        print()
+        print(get_data_correct_format(item[i]))
 
 
 if __name__ == "__main__":
